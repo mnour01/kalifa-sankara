30209c01dab140015ebe0e584796e74a7e082178
Mehdi Nour <mehdi.nour@altergaz.local>
2025-04-09 17:20:41 +0200

    fix up the page presentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mnour01.github.io/kalifa-sankara", // remplace TON-NOM par ton username GitHub
+  output: "static",
+});
