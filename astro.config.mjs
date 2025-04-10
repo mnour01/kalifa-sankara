44d2a44309b31de344d669a21d14a4463e8ad9f0
Mehdi Nour <mehdi.nour@altergaz.local>
2025-04-10 17:26:14 +0200

    add new pages, style ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mnour01.github.io/kalifa-sankara", // remplace TON-NOM par ton username GitHub
-  output: "static",
+  site: "https://mnour01.github.io/kalifa-sankara",
+  base: "/kalifa-sankara/",
+  output: "static"
 });
+
+
+
